7995f478a9a9c085d13d1f68b35382932f8afa0b
Mr3zf <ghanampwr@gmail.com>
2026-09-06 20:49:28 +0300

    Complete Azkar Telegram Bot project

---
+++ Main.py
@@ -0,0 +1,66 @@
+import telebot
+import threading
+import random
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+with open ('azkar.txt', 'r', encoding="utf-8") as file:
+    azkar = file.readlines()
+    azkar = [line.strip() for line in azkar if len(line.strip()) > 0]
+
+users_timers = {}
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'أهلًا انا بوت للأذكار 🤍 .\n حدد لي المدة وأنا اقوم بأرسال ذكر عشوائي بعد انتهاء كل مدة'
+                          '\nلمعرفة كيفية استخدامي ارسل /help ')
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message,'قم بكتابة /تذكير للبدء ,ثم ادخل عدد الدقائق بعد كل تذكير \n'
+                         'استخدم /قف لإيقاف التذكيرات \n'
+                         'منشئ البوت IG : @1dyd')
+@bot.message_handler(commands=['تذكير'])
+def ask(message):
+    bot.reply_to(message,'حدد المدة بالدقائق .'
+                         '\n مثل "15"')
+    bot.register_next_step_handler(message,check_num)
+
+def check_num(message):
+     try:
+         interval = int(message.text)
+         chat_id = message.chat.id
+         send_zkr(chat_id,interval)
+         start_sending(chat_id,interval)
+     except ValueError:
+         bot.reply_to(message,'حاول مره ثانية , أدخل فقط عدد الدقائق')
+         bot.register_next_step_handler(message,check_num)
+
+
+def send_zkr(chat_id,interval):
+    bot.send_chat_action(chat_id, 'typing')
+    bot.send_message(chat_id,f'سيتم ارسال اذكار متنوعة كل {interval} دقيقة 🤍')
+
+def start_sending(chat_id,interval):
+    zekr = random.choice(azkar)
+    bot.send_message(chat_id,zekr)
+    timer = threading.Timer(interval * 60 ,start_sending, args=(chat_id,interval))
+    timer.start()
+    users_timers[chat_id] = timer
+
+@bot.message_handler(commands=['قف'])
+def cancel(message):
+    chat_id = message.chat.id
+    if chat_id in users_timers:
+        users_timers[chat_id].cancel()
+        bot.send_message(chat_id, 'تم ايقاف التذكيرات')
+        users_timers.pop(chat_id)
+    else:
+        bot.reply_to(message,'لا يوجد تذكير لإيقافه')
+
+
+bot.polling()
