0330609464141a15560bd0a66aa121281b3d687e
Mr3zf <ghanampwr@gmail.com>
2026-09-08 03:28:24 +0300

    Add hours/minutes option and MongoDB user tracking

--- main.py
+++ main.py
@@ -20,7 +20,16 @@ with open ('azkar.txt', 'r', encoding="utf-8") as file:
 
 users_timers = {}
 
+def save_users(chat_id, amount, unit):
+    existing_users = users_db.find_one({'chat_id': chat_id})
+    if not existing_users:
+        users_db.insert_one({'chat_id': chat_id  , 'azkar_active': False, 'amount': amount, 'unit': unit })
 
+def update_amount(chat_id, amount,unit):
+    users_db.update_one({'chat_id': chat_id}, {'$set': {'amount': amount, 'unit': unit}})
+
+def update_users(chat_id, status):
+    users_db.update_one({'chat_id': chat_id}, {'$set': {'azkar_active': status}})
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -29,40 +38,63 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message,'قم بكتابة /تذكير للبدء ,ثم ادخل عدد الدقائق بعد كل تذكير \n'
-                         'استخدم /قف لإيقاف التذكيرات \n'
+    bot.reply_to(message,'قم بكتابة "/تذكير" فقط للبدء ,ثم حدد وحدة القياس بعد كل تذكير (دقائق , ساعات) \n'
+                         'استخدم "/قف" لإيقاف التذكيرات \n'
+                         '\n استخدم "/تعديل" لتعديل وقت التذكير'
                          'منشئ البوت IG : @1dyd')
 @bot.message_handler(commands=['تذكير'])
 def ask(message):
-    bot.reply_to(message,'حدد المدة بالدقائق .'
-                         '\n مثل "15"')
-    bot.register_next_step_handler(message,check_num)
+    bot.reply_to(message, 'كيف تريد ان يكون تذكيرك \n'
+                          '**ملاحظة**\n'
+                          'اكتب "دقائق" أو "ساعات"')
 
-def check_num(message):
+    bot.register_next_step_handler(message,ask_unit)
+
+def ask_unit(message):
+    unit = message.text.strip()
+    if unit not in ['ساعات', 'ساعة', 'ساعه', 'دقائق', 'دقايق']:
+        bot.reply_to(message,'اكتب "دقائق" أو "ساعات" فقط')
+        bot.register_next_step_handler(message,ask_unit)
+        return
+    bot.reply_to(message,f'حدد العدد المراد بال{unit}')
+    bot.register_next_step_handler(message,check_num,unit)
+
+def check_num(message,unit):
      try:
 
-         interval = int(message.text)
-         if interval <= 0:
+         amount = int(message.text)
+         if amount <= 0:
              bot.reply_to(message,"❌ ادخل رقم صحيح اكبر من 0")
-             bot.register_next_step_handler(message,check_num)
+             bot.register_next_step_handler(message,check_num,unit)
          else:
              chat_id = message.chat.id
-             send_zkr(chat_id, interval)
-             timer = threading.Timer(interval * 60, start_sending, args=(chat_id, interval))
+             if unit in ['ساعات', 'ساعة', 'ساعه']:
+                 seconds = amount * 3600
+             else:
+                 seconds = amount * 60
+
+             send_zkr(chat_id, amount,unit)
+
+             timer = threading.Timer(seconds, start_sending, args=(chat_id, seconds))
              timer.start()
+
              users_timers[chat_id] = timer
+
+             save_users(chat_id,amount, unit)
+
+             update_users(chat_id, True)
 
      except ValueError:
          bot.reply_to(message,'حاول مره ثانية , أدخل فقط عدد الدقائق')
-         bot.register_next_step_handler(message,check_num)
+         bot.register_next_step_handler(message,check_num ,unit)
 
 
-def send_zkr(chat_id,interval):
+def send_zkr(chat_id,amount,unit):
     bot.send_chat_action(chat_id, 'typing')
-    bot.send_message(chat_id,f'سيتم ارسال اذكار متنوعة كل {interval} دقيقة 🤍')
+    bot.send_message(chat_id,f' سيتم إرسال اذكار متنوعة كل {amount}{unit} 🤍.')
 
-def start_sending(chat_id,interval):
-    timer = threading.Timer(interval * 60 ,start_sending, args=(chat_id,interval))
+def start_sending(chat_id,seconds):
+    timer = threading.Timer(seconds ,start_sending, args=(chat_id,seconds))
     timer.start()
 
     zekr = random.choice(azkar)
@@ -73,50 +105,86 @@ def start_sending(chat_id,interval):
 
 @bot.message_handler(commands=['تعديل'])
 def edit_request(message):
-    bot.reply_to(message, 'أدخل الوقت الجديد لإرسال التذكيرات')
+    bot.reply_to(message, 'تفضِل ان يكون تذكيرك الجديد دقائق أو ساعات ؟')
 
+    bot.register_next_step_handler(message,edit_ask)
 
-    bot.register_next_step_handler(message,edit_timer)
+def edit_ask(message):
+    unit = message.text.strip()
+    if unit not in ["دقايق" , "دقائق" , "ساعات" , "ساعة" , "ساعه"]:
+        bot.reply_to(message,'❌❌ أكتب "دقائق" أو "ساعات"')
+        bot.register_next_step_handler(message,edit_ask)
+        return
+    bot.reply_to(message,f"أكتب رقم ال{unit} الجديد")
+    bot.register_next_step_handler(message,edit_timer,unit)
 
-def edit_timer(message):
+def edit_timer(message, unit):
     try:
-        interval = int(message.text)
-        if interval <= 0:
+        amount = int(message.text)
+        if amount <= 0:
             bot.reply_to(message, "❌❌ ارسل رقم صحيح مثل : 15")
-            bot.register_next_step_handler(message,edit_timer)
+            bot.register_next_step_handler(message, edit_timer, unit)
             return
 
         chat_id = message.chat.id
+
+        if unit in ['ساعات', 'ساعة', 'ساعه']:
+            seconds = amount * 3600
+        else:
+            seconds = amount * 60
 
         if chat_id in users_timers:
             users_timers[chat_id].cancel()
             users_timers.pop(chat_id)
         else:
-            bot.reply_to(message,'لايوجد تذكير لتعديله')
-            return 
+            bot.reply_to(message, 'لايوجد تذكير لتعديله')
+            return
 
-
-        timer = threading.Timer(interval * 60, start_sending, args=(chat_id, interval))
+        timer = threading.Timer(seconds, start_sending, args=(chat_id, seconds))
         timer.start()
 
+        update_amount(chat_id, amount, unit)
+        update_users(chat_id, True)
         users_timers[chat_id] = timer
 
-        bot.send_message(chat_id,f'تم تعديل التذكيرات \n'
-                             f'سيتم ارسال التذكير كل {interval} دقيقة 🤍')
+        bot.send_message(chat_id, f'تم تعديل التذكيرات \nسيتم ارسال التذكير كل {amount} {unit} 🤍')
 
     except ValueError:
-        bot.reply_to(message,"❌❌ ارسل رقم صحيح مثل : 15")
-        bot.register_next_step_handler(message,edit_timer)
+        bot.reply_to(message, "❌❌ ارسل رقم صحيح مثل : 15")
+        bot.register_next_step_handler(message, edit_timer, unit)
+
 
 @bot.message_handler(commands=['قف'])
 def cancel(message):
     chat_id = message.chat.id
     if chat_id in users_timers:
+
         users_timers[chat_id].cancel()
+
         bot.send_message(chat_id, 'تم ايقاف التذكيرات')
+
         users_timers.pop(chat_id)
+
+        update_users(chat_id, False)
     else:
         bot.reply_to(message,'لا يوجد تذكير لإيقافه')
 
+def restart_timers():
+    active_users = users_db.find({'azkar_active': True})
+    for user in active_users:
 
+        chat_id = user['chat_id']
+        amount = user['amount']
+        unit = user['unit']
+        if unit in ['ساعات', 'ساعة', 'ساعه']:
+            seconds = amount * 3600
+        else:
+            seconds = amount * 60
+
+        timer = threading.Timer(seconds, start_sending, args=(chat_id, seconds))
+        timer.start()
+
+        users_timers[chat_id] = timer
+
+restart_timers()
 bot.polling()
